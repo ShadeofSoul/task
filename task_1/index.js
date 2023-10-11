9fa53e91b189ca370e2acf5a4716bd852986b999
Shade Soul <aijanmoinokova@gmail.com>
2023-10-11 22:06:12 +0300

    task_1

--- task_1/index.js
+++ task_1/index.js
@@ -1,8 +1,33 @@
-import {encoded, translations} from './data.js'
+import { encoded, translations } from './data.js';
 
-console.log("Let's rock")
-console.log(encoded, translations)
+function decodeEncodedData(encoded, translations) {
+  return encoded.map(item => {
+    const decodedItem = { ...item };
 
+    for (const key in decodedItem) {
+      if (key.endsWith("Id") && !["groupId", "service", "formatSize", "ca"].includes(key)) {
+        const id = decodedItem[key];
+        if (translations[id] !== undefined) {
+          decodedItem[key] = translations[id];
+        }
+      }
+    }
 
+    return decodedItem;
+  });
+}
 
-// console.log(decoded)
+const decodedData = decodeEncodedData(encoded, translations);
+
+console.log("Decoded Data:");
+console.log(decodedData);
+
+const uniqueIds = encoded.reduce((acc, item) => {
+  const values = Object.values(item).filter(value => value !== null && !["groupId", "service", "formatSize", "ca"].includes(value));
+  return acc.concat(values);
+}, []);
+
+const uniqueIdsSet = new Set(uniqueIds);
+
+console.log("Unique IDs:");
+console.log([...uniqueIdsSet]);
